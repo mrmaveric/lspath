40b01fcd532cb470cd74cc8bff686be670d3d99e
James Bull <jpjbull@bcsonline.com.au>
2018-12-12 10:06:27 +1100

    Using skip function from STD to skip first arg

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use std::env;
 
+static VERSION: &str = "0.1.1";
+
 fn main() {
-	let paths = env::var_os("PATH").unwrap();
-	for path in env::split_paths(&paths) 
+	// Pointless command line arguements code
+	let args:Vec<String> = env::args().collect();
+
+	for arg in args.iter().skip(1) {
+		//if index == 0 {continue}
+		match arg as &str {
+			"-v"|"-V" => {println!("Version: {}", VERSION); return},
+			"-h"|"-H" => {println!("Usage: {} [-h help][-v version]", args[0]); return},
+			_ => (),
+		}
+	}
+	
+
+	// The actual code
+	for path in env::split_paths(&(env::var_os("PATH").unwrap()))
 	{
 		println!("{}", path.display());
 	}
