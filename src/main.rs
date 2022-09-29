83b2230bf20d46983af68cdd6e2ee3c69e9aed71
James Bull <jamesphillipbull@gmail.com>
2022-09-29 10:47:01 +1000

    Removed args variable and use env::args directly

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,11 @@ static VERSION: &str = "0.1.1";
 
 fn main() {
 	// Pointless command line arguements code
-	let args:Vec<String> = env::args().collect();
-
-	for arg in args.iter().skip(1) {
-		//if index == 0 {continue}
-		match arg as &str {
+	for arg in env::args().skip(1) {
+		match arg.as_str() {
 			"-v"|"-V" => {println!("Version: {}", VERSION); return},
-			"-h"|"-H" => {println!("Usage: {} [-h help][-v version]", args[0]); return},
-			_ => (),
+			"-h"|"-H" => {println!("Usage: {} [-h help][-v version]", env::args().nth(0).unwrap()); return},
+			_ => {println!("Unknown arguement: {}", arg); return},
 		}
 	}
 	
