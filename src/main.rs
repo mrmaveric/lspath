2aaa0a7cd8cfa1f95b4e262f2120c2058969f8f0
James Bull <jamesphillipbull@gmail.com>
2024-02-21 10:51:05 +1100

    Removing unwrap
    I'm still learning..

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,34 @@
 use std::env;
 
-const VERSION: &str = "0.1.1";
+const VERSION: &str = "0.1.2";
 
 fn main() {
-	// Pointless command line arguements code
-	for arg in env::args().skip(1) {
-		match arg.as_str() {
-			"-v"|"-V" => {println!("Version: {}", VERSION); return},
-			"-h"|"-H" => {println!("Usage: {} [-h help][-v version]", env::args().nth(0).unwrap()); return},
-			_ => {println!("Unknown arguement: {}", arg); return},
-		}
-	}
-	
+    // Pointless command line arguements code
+    for arg in env::args().skip(1) {
+        match arg.as_str() {
+            "-v" | "-V" => {
+                println!("Version: {}", VERSION);
+                return;
+            }
+            "-h" | "-H" => {
+                println!(
+                    "Usage: {} [-h help][-v version]",
+                    env::args().nth(0).unwrap()
+                );
+                return;
+            }
+            _ => {
+                println!("Unknown arguement: {}", arg);
+                return;
+            }
+        }
+    }
 
-	// The actual code
-	for path in env::split_paths(&(env::var_os("PATH").unwrap()))
-	{
-		println!("{}", path.display());
-	}
+    if let Some(path_list) = env::var_os("PATH") {
+        for path in env::split_paths(&path_list) {
+            println!("{}", path.display());
+        }
+    } else {
+        println!("PATH environmental variable is not set");
+    }
 }
