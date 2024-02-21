1801d330c0c42425ac4bdb737ba1affe622aee94
James Bull <jamesphillipbull@gmail.com>
2024-02-21 14:31:36 +1100

    Removing useless command line arguement stuff
    I don't need it.
    
    I find a reason to have command line arguements I'll do it properly with
    an external crate. Probably clap.

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,6 @@
 use std::env;
 
-const VERSION: &str = "0.1.2";
-
 fn main() {
-    // Pointless command line arguements code
-    for arg in env::args().skip(1) {
-        match arg.as_str() {
-            "-v" | "-V" => {
-                println!("Version: {}", VERSION);
-                return;
-            }
-            "-h" | "-H" => {
-                println!(
-                    "Usage: {} [-h help][-v version]",
-                    env::args().nth(0).unwrap()
-                );
-                return;
-            }
-            _ => {
-                println!("Unknown arguement: {}", arg);
-                return;
-            }
-        }
-    }
-
     let path_list = match env::var_os("PATH") {
         Some(pl) => pl,
         None => {
